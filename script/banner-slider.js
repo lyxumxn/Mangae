fddbb06c3eb22610b97d3bfb00795018047431c1
lyxumxn <isumin7393@gmail.com>
2026-08-19 14:45:13 +0900

    태그 줄바꿈

--- script/banner-slider.js
+++ script/banner-slider.js
@@ -3,32 +3,32 @@ const menuButton = document.querySelector(".menu-trigger");
 const categoryButtons = document.querySelectorAll(".category-button");
 
 menuButton.addEventListener("click", function () {
-  header.classList.toggle("menu-open");
+    header.classList.toggle("menu-open");
 });
 
 categoryButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const menuItem = button.parentElement;
-    const category = menuItem.nextElementSibling;
-    const categoryDetail = category.querySelector("details");
+    button.addEventListener("click", function () {
+        const menuItem = button.parentElement;
+        const category = menuItem.nextElementSibling;
+        const categoryDetail = category.querySelector("details");
 
-    category.classList.toggle("open");
-    categoryDetail.open = category.classList.contains("open");
-  });
+        category.classList.toggle("open");
+        categoryDetail.open = category.classList.contains("open");
+    });
 });
-document.querySelectorAll('.banner.swiper').forEach((banner) => {
-  new Swiper(banner, {
-    loop: true,
-    speed: 700,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-    },
-    pagination: {
-      el: banner.querySelector('.swiper-pagination'),
-      clickable: true,
-    },
-    keyboard: { enabled: true },
-  });
+document.querySelectorAll(".banner.swiper").forEach((banner) => {
+    new Swiper(banner, {
+        loop: true,
+        speed: 700,
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        pagination: {
+            el: banner.querySelector(".swiper-pagination"),
+            clickable: true,
+        },
+        keyboard: { enabled: true },
+    });
 });
